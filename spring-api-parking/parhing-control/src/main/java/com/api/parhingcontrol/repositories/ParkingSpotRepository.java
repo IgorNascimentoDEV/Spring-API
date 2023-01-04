3c94f35583542cd1da7ea8ae4a021e07bfe461c1
igor <diegoigor2001@gmail.com>
2023-01-04 12:49:08 -0300

    arquivo de anotações

---
+++ spring-api-parking/parhing-control/src/main/java/com/api/parhingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,18 @@
+package com.api.parhingcontrol.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parhingcontrol.models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+
+	 boolean existsByLicensePlateCar(String licensePlateCar);
+	 
+	 boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	 
+	 boolean existsByApartmentAndBlock(String apartment, String block);
+}
---
+++ spring-api-parking/parhing-control/src/main/java/com/api/parhingcontrol/services/ParkingSportService.java
@@ -0,0 +1,63 @@
+package com.api.parhingcontrol.services;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.api.parhingcontrol.models.ParkingSpotModel;
+import com.api.parhingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class ParkingSportService {
+
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSportService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		
+		return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
+
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
+	      return parkingSpotRepository.findAll(pageable);
+	  }
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+
+	
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
+
+
+}
